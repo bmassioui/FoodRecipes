c34622bf3228a5107acfe0578877796c729705e7
bouchaib-massioui <bouchaib.massioui@gmail.com>
2021-06-03 13:46:39 -0700

    create ui & classes

---
+++ app/src/main/java/com/bob/foodrecipes/ToolbarActivity.java
@@ -0,0 +1,14 @@
+package com.bob.foodrecipes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ToolbarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toolbar);
+    }
+}
---
+++ app/src/main/java/com/bob/foodrecipes/ViewRecipeActivity.java
@@ -0,0 +1,14 @@
+package com.bob.foodrecipes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewRecipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_recipe);
+    }
+}
